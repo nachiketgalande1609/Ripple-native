c55fde4a252bf3ae225c77cf3c6e61830736492e
Nachiket Galande <nachiketgalande1609@gmail.com>
2025-02-13 19:21:23 +0530

    Layout and navbar set

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,49 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function TabLayout() {
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarStyle: { backgroundColor: "#fff", height: 60 },
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ color, size }) => <Ionicons name="home-outline" size={size} color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="pages/search"
+                options={{
+                    title: "Search",
+                    tabBarIcon: ({ color, size }) => <Ionicons name="search-outline" size={size} color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="pages/messages"
+                options={{
+                    title: "Messages",
+                    tabBarIcon: ({ color, size }) => <Ionicons name="chatbubble-outline" size={size} color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="pages/notifications"
+                options={{
+                    title: "Notifications",
+                    tabBarIcon: ({ color, size }) => <Ionicons name="notifications-outline" size={size} color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="pages/profile"
+                options={{
+                    title: "Profile",
+                    tabBarIcon: ({ color, size }) => <Ionicons name="person-outline" size={size} color={color} />,
+                }}
+            />
+        </Tabs>
+    );
 }
