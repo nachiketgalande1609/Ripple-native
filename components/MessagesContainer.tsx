b4be2310e0d5c64567881383a312364e5c683c41
Nachiket Galande <nachiketgalande1609@gmail.com>
2025-02-17 01:22:29 +0530

    Message Details Drawer complete

--- components/MessagesContainer.tsx
+++ components/MessagesContainer.tsx
@@ -1,17 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import useAuthStore from "@/store/authStore";
-import {
-    View,
-    FlatList,
-    Text,
-    StyleSheet,
-    Image,
-    TouchableWithoutFeedback,
-    KeyboardAvoidingView,
-    Platform,
-    Keyboard,
-    TouchableOpacity,
-} from "react-native";
+import { View, FlatList, Text, StyleSheet, TouchableWithoutFeedback, KeyboardAvoidingView, Platform, Keyboard, TouchableOpacity } from "react-native";
 import MessageInput from "./MessageInput";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import Modal from "react-native-modal";
@@ -21,11 +10,8 @@ type Message = {
     sender_id: number;
     message_text: string;
     timestamp: string;
-    delivered?: boolean;
-    read?: boolean;
     delivered_timestamp?: string | null;
     read_timestamp?: string | null;
-    file_url?: string | null;
 };
 
 type MessagesType = Record<string, Message[]>;
@@ -82,14 +68,7 @@ export default function MessagesContainer({ messages, selectedUser, inputMessage
                                 <TouchableWithoutFeedback onLongPress={() => handleLongPress(item)}>
                                     <View style={[styles.messageWrapper, isCurrentUser && styles.currentUserWrapper]}>
                                         <View style={[styles.messageItem, isCurrentUser ? styles.currentUserMessage : styles.otherUserMessage]}>
-                                            {item.file_url ? (
-                                                <>
-                                                    <Image source={{ uri: item.file_url }} style={styles.messageImage} resizeMode="contain" />
-                                                    <Text style={styles.messageText}>{item.message_text}</Text>
-                                                </>
-                                            ) : (
-                                                <Text style={styles.messageText}>{item.message_text}</Text>
-                                            )}
+                                            <Text style={styles.messageText}>{item.message_text}</Text>
                                             <Text style={styles.timestampText}>
                                                 {new Date(item.timestamp).toLocaleTimeString([], {
                                                     hour: "2-digit",
@@ -115,20 +94,64 @@ export default function MessagesContainer({ messages, selectedUser, inputMessage
                         style={styles.drawerModal}
                     >
                         <View style={styles.drawerContainer}>
-                            <TouchableOpacity onPress={() => setDrawerOpen(false)} style={styles.closeButton}>
-                                <MaterialCommunityIcons name="close" size={20} color="#fff" />
-                            </TouchableOpacity>
-                            <Text style={styles.drawerTitle}>Message Details</Text>
+                            <View style={styles.messageDetailsHeader}>
+                                <Text style={styles.drawerTitle}>Message Details</Text>
+                                <TouchableOpacity onPress={() => setDrawerOpen(false)}>
+                                    <MaterialCommunityIcons name="close" size={20} color="#fff" />
+                                </TouchableOpacity>
+                            </View>
                             {selectedMessage && (
                                 <>
-                                    <Text style={styles.drawerText}>Sent: {new Date(selectedMessage.timestamp).toLocaleString()}</Text>
+                                    <View style={styles.detailBox}>
+                                        <MaterialCommunityIcons name="check" size={18} color="#fff" />
+                                        <View>
+                                            <Text style={styles.statusLabel}>Sent</Text>
+                                            <Text style={styles.detailText}>
+                                                {new Date(selectedMessage.timestamp).toLocaleString("en-GB", {
+                                                    day: "numeric",
+                                                    month: "short",
+                                                    hour: "2-digit",
+                                                    minute: "2-digit",
+                                                    hour12: true,
+                                                })}
+                                            </Text>
+                                        </View>
+                                    </View>
+
                                     {selectedMessage.delivered_timestamp && (
-                                        <Text style={styles.drawerText}>
-                                            Delivered: {new Date(selectedMessage.delivered_timestamp).toLocaleString()}
-                                        </Text>
+                                        <View style={styles.detailBox}>
+                                            <MaterialCommunityIcons name="check-all" size={18} color="#ccc" />
+                                            <View>
+                                                <Text style={styles.statusLabel}>Delivered</Text>
+                                                <Text style={styles.detailText}>
+                                                    {new Date(selectedMessage.delivered_timestamp).toLocaleString("en-GB", {
+                                                        day: "numeric",
+                                                        month: "short",
+                                                        hour: "2-digit",
+                                                        minute: "2-digit",
+                                                        hour12: true,
+                                                    })}
+                                                </Text>
+                                            </View>
+                                        </View>
                                     )}
+
                                     {selectedMessage.read_timestamp && (
-                                        <Text style={styles.drawerText}>Read: {new Date(selectedMessage.read_timestamp).toLocaleString()}</Text>
+                                        <View style={styles.detailBox}>
+                                            <MaterialCommunityIcons name="check-all" size={18} color="#38acff" />
+                                            <View>
+                                                <Text style={styles.statusLabel}>Read</Text>
+                                                <Text style={styles.detailText}>
+                                                    {new Date(selectedMessage.read_timestamp).toLocaleString("en-GB", {
+                                                        day: "numeric",
+                                                        month: "short",
+                                                        hour: "2-digit",
+                                                        minute: "2-digit",
+                                                        hour12: true,
+                                                    })}
+                                                </Text>
+                                            </View>
+                                        </View>
                                     )}
                                 </>
                             )}
@@ -151,14 +174,8 @@ const styles = StyleSheet.create({
         alignSelf: "flex-end",
     },
     messageItem: {
-        paddingVertical: 8,
-        paddingHorizontal: 10,
+        padding: 10,
         borderRadius: 10,
-    },
-    messageImage: {
-        borderRadius: 10,
-        height: 300,
-        width: "100%",
     },
     timestampText: {
         fontSize: 12,
@@ -182,10 +199,10 @@ const styles = StyleSheet.create({
         alignItems: "flex-end",
     },
     drawerContainer: {
-        width: "65%",
-        backgroundColor: "#222",
+        width: "55%",
+        backgroundColor: "#202327",
         height: "100%",
-        padding: 20,
+        padding: 10,
         borderTopLeftRadius: 10,
         borderBottomLeftRadius: 10,
     },
@@ -193,14 +210,30 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: "bold",
         color: "#fff",
-        marginBottom: 10,
     },
-    drawerText: {
+    detailBox: {
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: "hsl(214, 10%, 11%)",
+        padding: 12,
+        borderRadius: 10,
+        marginBottom: 10,
+        gap: 10,
+    },
+    statusLabel: {
+        fontWeight: "bold",
+        color: "#fff",
+        marginBottom: 4,
+    },
+    detailText: {
         color: "#bbb",
         fontSize: 14,
-        marginBottom: 5,
     },
-    closeButton: {
-        alignSelf: "flex-end",
+    messageDetailsHeader: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        flexDirection: "row",
+        paddingVertical: 12,
     },
 });
